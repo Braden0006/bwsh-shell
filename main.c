2926bb731ddb4e6846dc8c9cff05887d256c6180
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2025-03-10 14:11:00 -0600

    first commit, switched IDE's from Neovim to CLion

---
+++ main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/syslimits.h>
+#include <dirent.h>
+
+void show_files() {
+	DIR *d;
+	struct dirent *dir;
+
+	d = opendir(".");
+	if (d) {
+		while ((dir = readdir(d)) != NULL) {
+			if (dir->d_type == DT_REG) {
+				printf("%s\n", dir->d_name);
+			}
+		}
+
+		closedir(d);
+	}
+}
+
+int main()
+{
+	char command[50];
+	char target[] = "quit";
+
+	char *built_commands[] = {"ls", "cd"};
+
+	// Infinite loop
+	for (;;) {
+		printf("$ ");
+		scanf("%s", command);
+
+		// Checks if the input string is equal to "quit"
+		if (strcmp(command, target) == 0) {
+			break;
+		}
+		else {
+			for (int i = 0; i < 2; i++) {
+				if (strcmp(command, built_commands[i]) == 0) {
+					if (strcmp(built_commands[i], built_commands[0]) == 0) {
+						show_files();
+					}
+					break;
+				} else {
+					printf("This is not a built-in command!\n");
+				}
+			}
+		}
+	}
+
+	return 0;
+}
