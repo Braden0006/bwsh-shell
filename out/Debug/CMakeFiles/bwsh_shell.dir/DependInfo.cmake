78d9c900b7e0d77f90dfb22583a6bb46322d6fe5
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2025-03-23 07:03:11 -0600

    Changed files for building the project

---
+++ out/Debug/CMakeFiles/bwsh_shell.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/bradenwhitcher/workspace/github.com/Braden0006/bwsh-shell/create_process.c" "CMakeFiles/bwsh_shell.dir/create_process.c.o" "gcc" "CMakeFiles/bwsh_shell.dir/create_process.c.o.d"
+  "/Users/bradenwhitcher/workspace/github.com/Braden0006/bwsh-shell/history.c" "CMakeFiles/bwsh_shell.dir/history.c.o" "gcc" "CMakeFiles/bwsh_shell.dir/history.c.o.d"
+  "/Users/bradenwhitcher/workspace/github.com/Braden0006/bwsh-shell/linkedList.c" "CMakeFiles/bwsh_shell.dir/linkedList.c.o" "gcc" "CMakeFiles/bwsh_shell.dir/linkedList.c.o.d"
+  "/Users/bradenwhitcher/workspace/github.com/Braden0006/bwsh-shell/main.c" "CMakeFiles/bwsh_shell.dir/main.c.o" "gcc" "CMakeFiles/bwsh_shell.dir/main.c.o.d"
+  "/Users/bradenwhitcher/workspace/github.com/Braden0006/bwsh-shell/shellLoop.c" "CMakeFiles/bwsh_shell.dir/shellLoop.c.o" "gcc" "CMakeFiles/bwsh_shell.dir/shellLoop.c.o.d"
+  "/Users/bradenwhitcher/workspace/github.com/Braden0006/bwsh-shell/tokenize_line.c" "CMakeFiles/bwsh_shell.dir/tokenize_line.c.o" "gcc" "CMakeFiles/bwsh_shell.dir/tokenize_line.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ out/Debug/CMakeFiles/bwsh_shell.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bwsh_shell.dir/create_process.c.o"
+  "CMakeFiles/bwsh_shell.dir/create_process.c.o.d"
+  "CMakeFiles/bwsh_shell.dir/history.c.o"
+  "CMakeFiles/bwsh_shell.dir/history.c.o.d"
+  "CMakeFiles/bwsh_shell.dir/linkedList.c.o"
+  "CMakeFiles/bwsh_shell.dir/linkedList.c.o.d"
+  "CMakeFiles/bwsh_shell.dir/main.c.o"
+  "CMakeFiles/bwsh_shell.dir/main.c.o.d"
+  "CMakeFiles/bwsh_shell.dir/shellLoop.c.o"
+  "CMakeFiles/bwsh_shell.dir/shellLoop.c.o.d"
+  "CMakeFiles/bwsh_shell.dir/tokenize_line.c.o"
+  "CMakeFiles/bwsh_shell.dir/tokenize_line.c.o.d"
+  "bwsh_shell"
+  "bwsh_shell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bwsh_shell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
