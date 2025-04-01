61a2e1bc466d5abf1790b668cebb9a02aac78755
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2025-04-01 04:55:44 -0600

    Successfully created sqlite3 database

--- main.c
+++ main.c
@@ -4,11 +4,25 @@
 #include <sys/syslimits.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <sqlite3.h>
 
 #include "shell_functions.h"
 
 int main()
 {
+	sqlite3 *db;
+	char *zErrMsg = 0;
+	int rc;
+
+	rc = sqlite3_open("test.db", &db);
+
+	if (rc) {
+		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+		return 0;
+	} else {
+		fprintf(stderr, "Database opened successfully!\n");
+	}
+
 	char *commands[] = {"ls", "cd", "history", "alias", "pwd", "clear"};
 	char *command = (char *)malloc(50 * sizeof(char));
 
@@ -29,6 +43,8 @@ int main()
 
 	// Deallocates memory for the linked list
 	free_list(list);
+
+	sqlite3_close(db);
 
 	return 0;
 }
